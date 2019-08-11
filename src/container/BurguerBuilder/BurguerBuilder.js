7489aff765d129b3177ff10fd79b509637f542e9
Vinicius Sant'Ana <vinicius.santana@meupag.com.br>
2019-08-11 08:47:45 -0300

    Ajustando botão 'order now'

--- src/container/BurguerBuilder/BurguerBuilder.js
+++ src/container/BurguerBuilder/BurguerBuilder.js
@@ -13,7 +13,6 @@ import * as actions from '../../store/actions';
 class BurguerBuilder extends Component{
     
     state = {
-        purchaseable: false,
         purchasing: false,
         loading: false,
         error: false
@@ -40,7 +39,7 @@ class BurguerBuilder extends Component{
                     .map(igKey => ingredients[igKey])
                     .reduce((sum, el)=> sum + el, 0);
 
-        this.setState({purchaseable: sum>0})
+       return sum>0
     }
 
     purchaseCancelHandler = ()=>{
@@ -87,7 +86,7 @@ class BurguerBuilder extends Component{
                         ingredientRemoved = {this.props.onIngredientRemoved}
                         disabled={disableInfo}
                         price={this.props.price}
-                        purchaseable={this.state.purchaseable}
+                        purchaseable={this.updatePurchaseState(this.props.ings)}
                         ordered = {this.purchaseHandler}
                         />
                 </Fragment>
