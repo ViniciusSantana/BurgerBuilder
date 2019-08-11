81adbdad377ea22ed9aebd6538d1909ec1b53779
Vinicius Sant'Ana <vinicius.santana@meupag.com.br>
2019-08-11 08:08:41 -0300

    configurando redux

---
+++ src/store/reducer.js
@@ -0,0 +1,13 @@
+import * as actions from './actions'
+
+const initialState ={
+    ingredients: null,
+    totalPrice:4
+}
+
+const reducer = (state = initialState, action) =>{
+
+    return state;
+}
+
+export default reducer;
